564aa906daceb272ea0cf3d38596acbc3d8351a7
bayu@bti.id <bayu@bti.id>
2023-11-10 23:07:25 +0700

    feat: slicing signup form

--- src/auth/forms/SignupForm.tsx
+++ src/auth/forms/SignupForm.tsx
@@ -1,5 +1,151 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { SignupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { createUserAccount } from "@/lib/app-write/api";
+
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const isLoading = false;
+
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+
+    console.log(newUser);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="Brand SnapGram" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use SnapGram, please enter your details
+        </p>
+
+        <form
+          className="flex flex-col gap-5 w-full mt-4"
+          onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    className="shad-input"
+                    type="text"
+                    placeholder="shadcn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    className="shad-input"
+                    type="text"
+                    placeholder="shadcn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    className="shad-input"
+                    type="email"
+                    placeholder="shadcn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    className="shad-input"
+                    type="password"
+                    placeholder="shadcn"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="shad-button_primary" type="submit">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+              </div>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already Have Account ?
+            <Link
+              className="text-primary-500 text-small-semibold ml-1"
+              to="/sign-in">
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
 };
 
 export default SignupForm;
